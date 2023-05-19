84d5702091ed06938e96a8468f3722fa6e9263e4
palomalindinha01 <palomalindinha35@gmail.com>
2023-05-19 08:56:17 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function nome(){
+    let nome = 'paloma'
+    document.write(nome)
+console.log(nome);
+}
+
+function somar(){
+    let a = 5;
+    let b = 5;
+
+    let c = a+b;
+    console.log(c)
+    document.getElementById('valor').innerHTML = c
+}
